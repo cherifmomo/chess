79282cacd9493228887162e615a5d641e18f09e4
cherifmomo <cherifmohammed@gmail.com>
2021-01-19 21:58:22 +0100

    second version

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,24 +1,19 @@
 package com.company;
 
 public class Board {
+
+    PChess pChess;
     Case[][] cases = new Case[8][8];
-    public void initializer(){
-        int cols=8; //case column
-        int rows=8;// case rank
-          for(int i=0;i<cols;i++){
-              for(int j=0;j<rows;j++){
-                  int x=i; // x-coordinate of the rectangle by default
-                  int y=j; // y-coordinate of the rectangle by default
-                  if ((i+j) % 2 == 0) { // excellent
-                     this.cases[i][j]=1;
-                  } else {
-                      this.cases[i][j]=0;
-                  }
-                  //   rect(x, y, squareSize, squareSize);
-              }
-          }
 
-
+    public Board(PChess pChess) {
+        this.pChess = pChess;
+        for( int x=0; x<8; x++){
+            for( int y=0; y<8; y++) {
+                cases[x][y]=new Case();
+            }
+        }
     }
+
+
 
 }
--- src/com/company/Case.java
+++ src/com/company/Case.java
@@ -1,15 +1,23 @@
 package com.company;
 
 public class Case {
-    char rank;
-    char column;
+    char rank; // 1-8
+    char column; //a -h
     Piece piece;
 
 
-    public Case() {
-           piece= new Piece(this);
+    public boolean isBlack() {
+          return ( getX() +  getY() ) % 2 ==1 ;
 
     }
+    public int getX(){
+        return rank - '1';
+    }
+
+    public int getY(){
+        return rank - 'a';
+    }
+
 
 
 }
